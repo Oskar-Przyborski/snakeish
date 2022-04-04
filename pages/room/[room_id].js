466bec658206dbf444f90a80b4ccf3327122a49b
Oskar-Przyborski <oskar.przski@gmail.com>
2022-04-04 17:57:53 +0200

    added arrows to mobile and directions are safe

--- pages/room/[room_id].js
+++ pages/room/[room_id].js
@@ -1,9 +1,10 @@
-import { useEffect} from "react";
+import { useEffect } from "react";
 import { io } from 'socket.io-client'
+import styles from "../../styles/arrows.module.css"
 export default function Room({ room_id }) {
     useEffect(() => {
-        const socket = io("https://snakeish-backend.herokuapp.com/rooms");
-        //const socket = io("http://localhost:8080/rooms");
+        //const socket = io("https://snakeish-backend.herokuapp.com/rooms");
+        const socket = io("http://localhost:8080/rooms");
         socket.on("connect", () => {
             socket.removeAllListeners()
             socket.emit('join-room', room_id);
@@ -14,15 +15,7 @@ export default function Room({ room_id }) {
             const canvas = document.getElementById("canvas");
             const ctx = canvas.getContext("2d");
 
-            class Vector2 {
-                x;
-                y;
-                constructor(x, y) {
-                    this.x = x;
-                    this.y = y;
-                }
-            }
-            let direction = new Vector2(1, 0);
+            let direction = "right";
             let snake = [];
             let otherPlayers = [];
             let apple;
@@ -85,29 +78,48 @@ export default function Room({ room_id }) {
             })
 
             function SendTargetDirection(targetDirection) {
-                socket.emit('update-target-direction', { x: targetDirection.x, y: targetDirection.y });
+                socket.emit('update-target-direction', targetDirection);
             }
 
             //add keyboard input 
             document.addEventListener("keydown", (e) => {
                 const key = e.key;
-                if ((key == "ArrowUp" || key == "w") && direction.y != 1) {
-                    SendTargetDirection(new Vector2(0, -1));
-                } else if ((key == "ArrowDown" || key == "s") && direction.y != -1) {
-                    SendTargetDirection(new Vector2(0, 1));
-                } else if ((key == "ArrowLeft" || key == "a") && direction.x != 1) {
-                    SendTargetDirection(new Vector2(-1, 0));
-                } else if ((key == "ArrowRight" || key == "d") && direction.x != -1) {
-                    SendTargetDirection(new Vector2(1, 0));
+                if ((key == "ArrowUp" || key == "w") && direction != "down") {
+                    SendTargetDirection("up");
+                } else if ((key == "ArrowDown" || key == "s") && direction != "up") {
+                    SendTargetDirection("down");
+                } else if ((key == "ArrowLeft" || key == "a") && direction != "right") {
+                    SendTargetDirection("left");
+                } else if ((key == "ArrowRight" || key == "d") && direction != "left") {
+                    SendTargetDirection("right");
                 }
             });
+            //if mobile - show touch controls
+            function isMobile() {
+                return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+            }
+            if (isMobile()) {
+                document.getElementById("move-up").style.display = "block";
+                document.getElementById("move-up").addEventListener("click", () => { direction != "down" && SendTargetDirection("up") });
+                document.getElementById("move-down").style.display = "block";
+                document.getElementById("move-down").addEventListener("click", () => { direction != "up" && SendTargetDirection("down") });
+                document.getElementById("move-left").style.display = "block";
+                document.getElementById("move-left").addEventListener("click", () => { direction != "right" && SendTargetDirection("left") });
+                document.getElementById("move-right").style.display = "block";
+                document.getElementById("move-right").addEventListener("click", () => { direction != "left" && SendTargetDirection("right") });
+            }
         })
     }, [])
-
     return (<>
         <h1>Room {room_id}</h1>
         <canvas id="canvas" width={500} height={500}></canvas>
         <h2 id="score">Score: </h2>
+        <div id="touch-controls">
+            <button id="move-up" className={styles.moveUp + " "+styles.arrow} style={{ display: "none" }}>Up</button>
+            <button id="move-down" className={styles.moveDown + " "+styles.arrow} style={{ display: "none" }}>Down</button>
+            <button id="move-left" className={styles.moveLeft + " "+styles.arrow}style={{ display: "none" }}>Left</button>
+            <button id="move-right" className={styles.moveRight + " "+styles.arrow} style={{ display: "none" }}>Right</button>
+        </div>
     </>)
 }
 
