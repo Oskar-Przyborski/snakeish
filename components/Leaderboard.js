ac6efac1a5112d2f2d74ef00a6c6ea86e5cd0add
Oskar-Przyborski <oskar.przski@gmail.com>
2022-04-11 16:59:16 +0200

    leaderboard is sorted

--- components/Leaderboard.js
+++ components/Leaderboard.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUser } from "@fortawesome/free-solid-svg-icons"
 import { Title } from "../styles/styled-components"
 export default function Leaderboard(props) {
+    const sortedPlayers = props.players.sort((a, b) => b.gameData.score - a.gameData.score);
     return (<div>
         <Title center responsive>Leaderboard</Title>
         <table style={{ width: "100%", textAlign: "center", fontSize: "1.3em" }}>
@@ -13,7 +14,7 @@ export default function Leaderboard(props) {
             </thead>
             <tbody>
                 {
-                    props.players.map((player, idx) => {
+                    sortedPlayers.map((player, idx) => {
                         return <tr key={idx}>
                             <td>{(props.socketID == player.socketID ? <FontAwesomeIcon icon={faUser} /> : "")} {player.gameData.name}</td>
                             <td>{player.gameData.score}</td>
