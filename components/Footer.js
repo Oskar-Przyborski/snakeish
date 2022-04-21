13be7f06985178606f8dc4bb6542997a268aae99
Oskar-Przyborski <oskar.przski@gmail.com>
2022-04-21 18:24:24 +0200

    adedd newline to footer github

--- components/Footer.js
+++ components/Footer.js
@@ -16,13 +16,13 @@ export default function Footer() {
     return (
         <FooterStyled>
             <div style={{ padding: "0.1em" }}>
-                <Flex justifyContent="center" align="center">
+                <Flex justifyContent="center" align="center" column>
                     <div>
                         <span style={{ fontWeight: "bold", margin: "0.2em 0.3em" }}>Snakeish</span>
                         by Oskar Przyborski
                     </div>
                     <div style={{ margin: "0.2em 1em" }} className="github-icon">
-                        <Link href="https://github.com/Oskar-Przyborski/snakeish" passHref><a target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} fontSize="1.3em" /></a></Link>
+                        <Link href="https://github.com/Oskar-Przyborski/snakeish" passHref><a target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} fontSize="1.3em" /> Github</a></Link>
                     </div>
                 </Flex>
             </div>
